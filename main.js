4fca4f70a7e6e2d02804da4ea330e51f6cb1c371
Ninodi <ninomachaidze16@gmail.com>
2023-10-13 23:27:43 +0400

    homework4

--- main.js
+++ main.js
@@ -1,76 +1,80 @@
 //ამოცანა N1
-user = [
-    {name: 'Temo', age: 25},
-    {name: 'Lasha', age: 21},
-    {name: 'Ana', age: 28},
-]
-
-function findYoungest(user){
-    let youngest = user[0]
-    for(singleUser of user){
-        if(singleUser.age < youngest.age){
-            youngest = singleUser
-        }
+function replaceChar(string, valueToReplace, valueToReplaceWith){
+    //ეს იმ შემთხვევაში თუ სხვა data type ების დაშვება არ გვინდა
+    if(![string, valueToReplace, valueToReplaceWith].every(eachVar => typeof eachVar === "string")){
+        console.log("Not a valid data type")
+        return
     }
 
-    return youngest.name
+
+    let modifiedString = string
+        .split('')
+        .map(char => char === valueToReplace ? char = valueToReplaceWith : char = char)
+        .join('')
+
+    console.log(modifiedString)
 }
 
-console.log(findYoungest(user))
+replaceChar("Hello Friends", "H", "u")
 
 
 //ამოცანა N2
 
-//2.1 პირველი ვარიანტი როდესაც ახალ მასივში ობიექტები გადადის რეფერენსის გარეშე
-let newArray1 = []
+//2.1 თუ წინადადებაში სიტყვების მხოლოდ პირველი ასოს გადაკეთება გვინდა
+ function firstCharToUpperCase(string){
+    if(typeof string !== 'string') return
 
-function getNewArray(user){
-    for(item of user){
-        newArray1.push({
-            name: item.name,
-            age: item.age
+    let modifiedString = string
+        .split(' ')
+        .map(word => {
+            let firstLetter = word[0].toUpperCase()
+            let restOfTheWord = word.slice(1)
+            return firstLetter + restOfTheWord
         })
+        .join(' ')
+
+
+    console.log(modifiedString)
+ }
+
+ firstCharToUpperCase("this is a solution for problem 2.1")
+
+
+ //2.2 თუ წინადადებაში სიტყვების ყველა ასოს გადაკეთება გვინდა
+
+ function allToUppercase(string){
+    let uppercase = string.toUpperCase()
+
+    console.log(uppercase)
+ }
+
+ allToUppercase("this is a solution of problem 2.2")
+
+
+ //ამოცანა N3
+
+ let users = [
+    {
+        name: 'Saba',
+        age: 20
+    },
+    {
+        name: 'Lizi',
+        age: 16
+    },
+    {
+        name: 'Lasha', 
+        age: 30
     }
+]
+
+
+function sortByAge(array){
+    array.sort((a, b) => a.age - b.age);
+    
+    console.log(array)
 }
 
-getNewArray(user)
-console.log(newArray1)
+sortByAge(users)
 
-//2.2 მეორე ვარიანტი როდესაც ახალ მასივში ობიექტები გადადის რეფერენსით
-let newArray2 = user.map(item => item)
-console.log(newArray2)
-
-
-//ამოცანა N3
-function diceGame(){
-    let a = singlePlayerMove()
-    let b = singlePlayerMove()
-
-    console.log('Attempt count for a is ' + a)
-    console.log('Attempt count for b is ' + b)
-
-    if(a > b){
-        return 'Winner is b'
-    }else if(b > a){
-        return 'Winner is a'
-    }else return 'Its a draw'
-}
-
-function singlePlayerMove(){
-    let numOfTries = 0
-    while(true){
-        let randomNum = getRandomNum()
-        numOfTries++
-        if(randomNum === 3){
-            return numOfTries
-        }
-    }
-}
-
-function getRandomNum(){
-    return Math.floor(Math.random() * 7);
-}
-
-let winner = diceGame()
-
-console.log(winner)
+    
