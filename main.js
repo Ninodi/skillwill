e1a82a5b6d6bcc80caf4760dba84e58418173860
Ninodi <ninomachaidze16@gmail.com>
2023-10-17 23:13:22 +0400

    homework5

--- main.js
+++ main.js
@@ -1,80 +1,121 @@
 //ამოცანა N1
-function replaceChar(string, valueToReplace, valueToReplaceWith){
-    //ეს იმ შემთხვევაში თუ სხვა data type ების დაშვება არ გვინდა
-    if(![string, valueToReplace, valueToReplaceWith].every(eachVar => typeof eachVar === "string")){
-        console.log("Not a valid data type")
-        return
-    }
+const textToHide = document.getElementById('text-to-hide')
+const hideBtn = document.getElementById('hide-btn') 
 
-
-    let modifiedString = string
-        .split('')
-        .map(char => char === valueToReplace ? char = valueToReplaceWith : char = char)
-        .join('')
-
-    console.log(modifiedString)
-}
-
-replaceChar("Hello Friends", "H", "u")
-
+hideBtn.addEventListener('click', () => {
+    textToHide.style.display = 'none'
+})
 
 //ამოცანა N2
+const containerTask2 = document.querySelector('.container-task-2')
 
-//2.1 თუ წინადადებაში სიტყვების მხოლოდ პირველი ასოს გადაკეთება გვინდა
- function firstCharToUpperCase(string){
-    if(typeof string !== 'string') return
+const link = document.createElement('a')
+link.setAttribute('href', '#')
+link.innerText = 'Go to profile'
 
-    let modifiedString = string
-        .split(' ')
-        .map(word => {
-            let firstLetter = word[0].toUpperCase()
-            let restOfTheWord = word.slice(1)
-            return firstLetter + restOfTheWord
-        })
-        .join(' ')
+const cardTitle = document.createElement('h2')
+cardTitle.innerText = "Gandalf"
 
+const card = document.createElement('div')
+card.id = 'card'
 
-    console.log(modifiedString)
- }
+card.append(cardTitle, link)
 
- firstCharToUpperCase("this is a solution for problem 2.1")
+containerTask2.appendChild(card)
 
 
- //2.2 თუ წინადადებაში სიტყვების ყველა ასოს გადაკეთება გვინდა
-
- function allToUppercase(string){
-    let uppercase = string.toUpperCase()
-
-    console.log(uppercase)
- }
-
- allToUppercase("this is a solution of problem 2.2")
-
-
- //ამოცანა N3
-
- let users = [
+//ამოცანა N3
+const quizQuestions = [
     {
-        name: 'Saba',
-        age: 20
+        question: "1) Which of the following command is used to create react-js-app ?",
+        answers: [
+            {ans: 'npm install create-react-app', correct: false},
+            {ans: 'npx create-react-app appname', correct: true},
+            {ans: 'npx install create-react-app -g', correct: false}
+        ]
     },
     {
-        name: 'Lizi',
-        age: 16
+        question: "2) In React.js which one of the following is used to create a class for Inheritance ?",
+        answers: [
+            {ans: 'Extends', correct: true},
+            {ans: 'Create', correct: false},
+            {ans: 'Inherits', correct: false},
+            {ans: 'Delete', correct: false},
+        ]
     },
     {
-        name: 'Lasha', 
-        age: 30
-    }
+        question: "3) What is the default port number in which the application run ?",
+        answers: [
+            {ans: '8080', correct: false},
+            {ans: '3000', correct: true}
+        ]
+    },
+    {
+        question: "4) Which of the following valid component return type of React ?",
+        answers: [
+            {ans: '5', correct: false},
+            {ans: '3', correct: false},
+            {ans: '1', correct: true}
+        ]
+    },
+    {
+        question: "5) Which of the following is a way to handle data in React.js ?",
+        answers: [
+            {ans: 'State & Component', correct: false},
+            {ans: 'State & Props', correct: true},
+            {ans: 'State & Services', correct: true}
+        ]
+    },
 ]
 
 
-function sortByAge(array){
-    array.sort((a, b) => a.age - b.age);
-    
-    console.log(array)
-}
+const quizContainer = document.getElementById('quizContainer')
+const pointsText = document.getElementById('points')
+let points = 0
 
-sortByAge(users)
+quizQuestions.map(eachQuestion => {
 
+    let quesContainer = document.createElement('div')
+    quesContainer.classList.add('question-container')
+
+    let question = document.createElement('div')
+    question.classList.add('question')
+    question.innerText = eachQuestion.question
+
+    let ansContainer = document.createElement('div')
+    ansContainer.classList.add('answers-container')
+
+    quesContainer.appendChild(question)
+
+
+    eachQuestion.answers.map(eachAnswer => {
+        let answer = document.createElement('div')
+        answer.classList.add('answer')
+        answer.setAttribute('isCorrect', `${eachAnswer.correct}`)
+        answer.innerText = eachAnswer.ans
     
+        ansContainer.append(answer)
+        quesContainer.append(ansContainer)
+
+    })
+
+    quizContainer.appendChild(quesContainer)
+})
+
+quizContainer.querySelectorAll('.answer').forEach(answerElement => {
+    answerElement.addEventListener('click', () => {
+        //Make elements unclickable after selecting an answer
+        answerElement.parentElement.style.pointerEvents = 'none'
+        
+        // Check if the answer is correct
+        let isCorrect = answerElement.getAttribute('isCorrect')
+
+        if(isCorrect === "true"){
+            answerElement.classList.add('correct')
+            points++
+            pointsText.innerText = points
+        }else{
+            answerElement.classList.add('false')
+        }
+    });
+});
